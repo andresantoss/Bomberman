9c187d9aaf706b210d62ee27658a384fe5ea52bd
Pedro Rafael Faria Ferreira <pedro.rafael@sempreceub.com>
2022-08-25 22:55:01 -0300

    Creating the Win State

--- Bomberman/Assets/Scripts/MovementController.cs
+++ Bomberman/Assets/Scripts/MovementController.cs
@@ -87,7 +87,7 @@ public class MovementController : MonoBehaviour
     private void OnDeathSequenceEnded()
     {
         gameObject.SetActive(false);
-        
+        FindObjectOfType<GameManager>().CheckWinState();
     }
 
 }
